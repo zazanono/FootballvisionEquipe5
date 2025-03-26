3ba8582667fef7d2e8badd8af3634364a19e4d23
zazanono <zacharie.noel@gmail.com>
2025-03-26 14:32:18 -0400

    aej

--- video_foot_ml/utils/__init__.py
+++ video_foot_ml/utils/__init__.py
@@ -1,2 +1,2 @@
-from .video_utils import lire_video,sauvegarder_video
-from .bbox_utils import get_center_of_bbox,get_bbox_width
+from .video_utils import lire_video, sauvegarder_video
+from .bbox_utils import get_center_of_bbox, get_bbox_width
