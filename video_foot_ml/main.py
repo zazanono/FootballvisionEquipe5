2d9324bffb2dd547f936b5039c43b1dfe779b9e4
zazanono <zacharie.noel@gmail.com>
2025-03-26 17:20:48 -0400

    proutz

--- video_foot_ml/main.py
+++ video_foot_ml/main.py
@@ -1,19 +1,21 @@
-from video_foot_ml.utils.video_utils import sauvegarder_video, lire_video
+from utils import sauvegarder_video, lire_video
 from trackers import Tracker
 
-def main(video_path):
+
+def main():
     # Lire la video
-    video_images = lire_video(video_path)
+    video_images = lire_video("video_foot_ml/input_videos/foot1.mp4")
 
     # Initialiser les trackers
-    tracker = Tracker('models/football-player-detector-n.pt')
-    tracks = tracker.get_object_tracks(video_images, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
+    tracker = Tracker('video_foot_ml/models/football-player-detector-n.pt')
+    tracks = tracker.get_object_tracks(video_images, read_from_stub=True,
+                                       stub_path='video_foot_ml/stubs/track_stubs.pkl')
 
     # Dessin video
     video_sortie_images = tracker.draw_annotations(video_images, tracks)
 
     # Sauvegarder la video
-    sauvegarder_video(video_sortie_images, "/output_videos_outputfoot.mp4", 2)
+    sauvegarder_video(video_sortie_images, "video_foot_ml/output_videos/", 2)
 
 
 if __name__ == '__main__':
