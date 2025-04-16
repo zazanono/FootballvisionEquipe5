00cc699fe975b40169453e59e9adc69c1cda67e5
ethanol2233 <6250090@bdeb.qc.ca>
2025-04-16 16:12:37 -0400

    liaison entre l'interface et l'ia

--- video_foot_ml/MainML.py
+++ video_foot_ml/MainML.py
@@ -20,21 +20,21 @@ def analyseYolo(chemin_vid, vid_deja_faite):
 
 
 
-#def main():
+def main():
     # Lire la video
-    #video_images = lire_video("input_videos/foot1.mp4")
+    video_images = lire_video("input_videos/foot1.mp4")
 
     # Initialiser les trackers
-    #tracker = Tracker('models/detecteur_foot_n.pt')
-    #tracks = tracker.get_object_tracks(video_images, read_from_stub=True,
-                                       #stub_path='stubs/track_stubs.pkl')
+    tracker = Tracker('models/detecteur_foot_n.pt')
+    tracks = tracker.get_object_tracks(video_images, read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl')
 
     # Dessin video
-    #video_sortie_images = tracker.draw_annotations(video_images, tracks)
+    video_sortie_images = tracker.draw_annotations(video_images, tracks)
 
     # Sauvegarder la video
-    #sauvegarder_video(video_sortie_images, "output_videos/", "outputtest")
+    sauvegarder_video(video_sortie_images, "output_videos/", "output_videos")
 
 
-#if __name__ == '__main__':
-    #main()
+if __name__ == '__main__':
+    main()
